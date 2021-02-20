9c76de05cda5b8e2673f986e4e85a8e6582ff611
Umair Karel <umair.karel@gmail.com>
2021-02-20 09:25:07 +0530

    Initial commit

--- To-Do App/ToDoApp/settings.py
+++ To-Do App/ToDoApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2=$gbsmdj9htz@i6=jmaka5ye+ptq&*@v&u!e#ebqnq__o_x5r'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
